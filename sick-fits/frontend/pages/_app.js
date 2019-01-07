2b41ee21f553fc383ea5153d8ede61977aa3c514
Aaron Precious <aaron@candyboxmarketing.com>
2019-01-07 16:04:11 -0500

    Added Styled Components and some comments to remember what some of the Next JS stuff is doing

--- sick-fits/frontend/pages/_app.js
+++ sick-fits/frontend/pages/_app.js
@@ -1,8 +1,12 @@
 import App, { Container } from "next/app";
 import Page from "../components/Page";
 
+
+//Custom app router to keep state between multiple pages
+
 class MyApp extends App {
   render() {
+    //Component should be page component (e.g. sell.js) determined based off of current route
     const { Component } = this.props;
 
     return (
