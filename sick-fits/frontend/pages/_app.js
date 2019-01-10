faee5456f9a93856d2c6a59ca68a41e4957eae75
Aaron Precious <aaron@candyboxmarketing.com>
2019-01-10 12:09:46 -0500

    Some extra styling to the Header

--- sick-fits/frontend/pages/_app.js
+++ sick-fits/frontend/pages/_app.js
@@ -1,9 +1,7 @@
 import App, { Container } from "next/app";
 import Page from "../components/Page";
 
-
 //Custom app router to keep state between multiple pages
-
 class MyApp extends App {
   render() {
     //Component should be page component (e.g. sell.js) determined based off of current route
