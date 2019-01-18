afaa6edc18e9939b48b938303f2fa140aeac2bce
Aaron Precious <aaron@candyboxmarketing.com>
2019-01-18 11:44:37 -0500

    Updates to backend for new Query/Mutation

--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -2,8 +2,8 @@ const { forwardTo } = require("prisma-binding");
 
 const Query = {
   //if query is same as in schema use this shortcut then write custom queries
-  items: forwardTo("db")
-
+  items: forwardTo("db"),
+  item: forwardTo("db")
   // async items(parent, args, ctx, info) {
   //   const items = await ctx.db.query.items();
   //   return items;
