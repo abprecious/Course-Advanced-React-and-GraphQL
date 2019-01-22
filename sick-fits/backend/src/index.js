c6dfc5726e420f19518847de8dda0ed7e29365e0
Aaron Precious <aaron@candyboxmarketing.com>
2019-01-22 16:44:50 -0500

    Prettier changes

--- sick-fits/backend/src/index.js
+++ sick-fits/backend/src/index.js
@@ -1,19 +1,22 @@
 // Start up Node server
 
-require('dotenv').config({ path: 'variables.env' });
-const createServer = require('./createServer');
-const db = require('./db');
+require("dotenv").config({ path: "variables.env" });
+const createServer = require("./createServer");
+const db = require("./db");
 
 const server = createServer();
 
 //TODO use express middleware to handle cookies (JWT)
 //TODO use express middleware to populate current user
 
-server.start({
-  cors: {
-    credentials: true,
-    origin: process.env.FRONTEND_URL,
+server.start(
+  {
+    cors: {
+      credentials: true,
+      origin: process.env.FRONTEND_URL
+    }
+  },
+  deets => {
+    console.log(`Server is now running on port http:/localhost:${deets.port}`);
   }
-}, deets => {
-  console.log(`Server is now running on port http:/localhost:${deets.port}`);
-})
+);
