29a1d9d08171c5e884f2085f743d70d69084d61a
Aaron Precious <aaron@candyboxmarketing.com>
2019-01-23 10:58:17 -0500

    Increased maxWidth in theme

--- sick-fits/frontend/components/Page.js
+++ sick-fits/frontend/components/Page.js
@@ -5,13 +5,13 @@ import Header from "./Header";
 import Meta from "./Meta";
 
 const theme = {
-  red: '#FF0000',
-  black: '#393939',
-  grey: '#3A3A3A',
-  lightGrey: '#E1E1E1',
-  offWhite: '#EDEDED',
-  maxWidth: '1000px',
-  bs: '0 12px 24px 0 rgba(0, 0, 0 , 0.89)',
+  red: "#FF0000",
+  black: "#393939",
+  grey: "#3A3A3A",
+  lightGrey: "#E1E1E1",
+  offWhite: "#EDEDED",
+  maxWidth: "1200px",
+  bs: "0 12px 24px 0 rgba(0, 0, 0 , 0.89)"
 };
 
 const StyledPage = styled.div`
@@ -62,9 +62,7 @@ class Page extends Component {
           <Meta />
           <Header />
           {/* Gets Component from App */}
-          <Inner>
-            {this.props.children}
-          </Inner>
+          <Inner>{this.props.children}</Inner>
         </StyledPage>
       </ThemeProvider>
     );
