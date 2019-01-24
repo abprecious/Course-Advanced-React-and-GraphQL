721b8b0f3020ce5697b49978c689b7f8dc90a807
Aaron Precious <aaron@candyboxmarketing.com>
2019-01-24 12:26:43 -0500

    Updates to backend for schema and resolvers for signin and signout

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -68,6 +68,33 @@ const Mutations = {
     });
     //finally we return the user to the browser
     return user;
+  },
+  async signin(parent, { email, password }, ctx, info) {
+    //check if there is a user with that email
+    const user = await ctx.db.query.user({ where: { email } });
+    if (!user) {
+      //up in the air if you should return that no email exists for said provided email
+      throw new Error(`No such user found for email ${email}`);
+    }
+    //check if their password is correct
+    const valid = await bcrypt.compare(password, user.password);
+    if (!valid) {
+      throw new Error("Invalid Password");
+    }
+    //generate JWT token
+    const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
+    //set cookie with token
+    ctx.response.cookie("token", token, {
+      //can't access via JS or bad extensions etc
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24 * 365 //1 year cookie
+    });
+    //return the user
+    return user;
+  },
+  signout(parents, args, ctx, info) {
+    ctx.response.clearCookie("token");
+    return { message: "Goodbye!" };
   }
 };
 
