21bb198b7bd8c118a1e6b65c8f661cf15dfba42c
Aaron Precious <aaron@candyboxmarketing.com>
2019-01-24 12:34:04 -0500

    Include mutation and refetching of CURRENT_USER_QUERY

--- sick-fits/frontend/components/Signout.js
+++ sick-fits/frontend/components/Signout.js
@@ -4,6 +4,21 @@ import gql from "graphql-tag";
 
 import { CURRENT_USER_QUERY } from "./User";
 
-const Signout = props => <button>Sign Out</button>;
+const SIGN_OUT_MUTATION = gql`
+  mutation SIGN_OUT_MUTATION {
+    signout {
+      message
+    }
+  }
+`;
+
+const Signout = props => (
+  <Mutation
+    mutation={SIGN_OUT_MUTATION}
+    refetchQueries={[{ query: CURRENT_USER_QUERY }]}
+  >
+    {signout => <button onClick={signout}>Sign Out</button>}
+  </Mutation>
+);
 
 export default Signout;
