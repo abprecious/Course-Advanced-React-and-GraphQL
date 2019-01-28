79835a6612c11502bd91e71b50d5e52c6ce639f9
Aaron Precious <aaron@candyboxmarketing.com>
2019-01-28 16:00:26 -0500

    Creation and mutation for CartItem

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -211,6 +211,31 @@ const Mutations = {
       },
       info
     );
+  },
+  async addToCart(parent, args, ctx, info) {
+    //check if singed in
+    const { userId } = ctx.request;
+    if (!user.Id) {
+      throw new Error("You must be signed in to add an item to your cart");
+    }
+    //query the users current cart
+    const [existingCartItem] = ctx.db.query.cartItems({
+      user: { id: userId },
+      item: { id: args.id }
+    });
+    //check if that item is already in their cart and increment by 1
+    if (existingCartItem) {
+      console.log("this item is already in their cart");
+      return ctx.db.mutation.updateCartItem({
+        where: { id: existingCartItem.id },
+        data: { quantity: existingCartItem.quantity + 1 }
+      });
+    }
+    //if not create a new cartItem for that user
+    return ctx.db.mutation.createCartItem({
+      user: { connect: { id: userId } },
+      item: { connect: { id: args.id } }
+    });
   }
 };
 
