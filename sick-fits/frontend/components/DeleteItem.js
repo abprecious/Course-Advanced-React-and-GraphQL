9053318154eb4b6c5ee701a53c950a5120da84d7
Aaron Precious <aaron@candyboxmarketing.com>
2019-01-29 14:53:47 -0500

    Added future TODO's

--- sick-fits/frontend/components/DeleteItem.js
+++ sick-fits/frontend/components/DeleteItem.js
@@ -12,6 +12,7 @@ const DELETE_ITEM_MUTATION = gql`
 `;
 
 class DeleteItem extends Component {
+  //TODO delete image from cloudinary as well
   update = (cache, payload) => {
     //manually update the cache on the client so it matches the server
     //1. Read the cache for the items we want
@@ -23,6 +24,7 @@ class DeleteItem extends Component {
     //3. Put the items back!
     cache.writeQuery({ query: ALL_ITEMS_QUERY, data });
   };
+  //TODO remove all cartItems containing deleted item
   render() {
     return (
       <Mutation
