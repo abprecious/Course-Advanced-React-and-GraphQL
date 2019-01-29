a321ae5462e6af755466eb06c0be4a031bfdf46a
Aaron Precious <aaron@candyboxmarketing.com>
2019-01-29 10:58:54 -0500

    Added in the RemoveFromCart component again

--- sick-fits/frontend/components/CartItem.js
+++ sick-fits/frontend/components/CartItem.js
@@ -1,7 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import PropTypes from "prop-types";
-// import RemoveFromCart from "./RemoveFromCart";
+import RemoveFromCart from "./RemoveFromCart";
 import formatMoney from "../lib/formatMoney";
 
 const CartItemStyles = styled.li`
@@ -32,7 +32,7 @@ const CartItem = ({ cartItem }) => (
         </em>
       </p>
     </div>
-    {/* <RemoveFromCart id={cartItem.id} /> */}
+    <RemoveFromCart id={cartItem.id} />
   </CartItemStyles>
 );
 
