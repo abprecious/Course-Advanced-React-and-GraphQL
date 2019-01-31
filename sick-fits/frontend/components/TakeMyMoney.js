42e7b29be0ca0f5ae10935f57bbcefc44f47b81e
Aaron Precious <aaron@candyboxmarketing.com>
2019-01-31 14:35:29 -0500

    close cart window and refactor with  Composed/react-adopt

--- sick-fits/frontend/components/TakeMyMoney.js
+++ sick-fits/frontend/components/TakeMyMoney.js
@@ -5,10 +5,12 @@ import Router from "next/router";
 import NProgress from "nprogress";
 import PropTypes from "prop-types";
 import gql from "graphql-tag";
+import { adopt } from "react-adopt";
 
 import calcTotalPrice from "../lib/calcTotalPrice";
 import Error from "./ErrorMessage";
 import User, { CURRENT_USER_QUERY } from "./User";
+import { TOGGLE_CART_MUTATION } from "../components/Cart";
 
 const CREATE_ORDER_MUTATION = gql`
   mutation createOder($token: String!) {
@@ -24,12 +26,29 @@ const CREATE_ORDER_MUTATION = gql`
   }
 `;
 
+const Composed = adopt({
+  user: ({ render }) => <User>{render}</User>,
+  toggleCart: ({ render }) => (
+    <Mutation mutation={TOGGLE_CART_MUTATION}>{render}</Mutation>
+  ),
+  createOrder: ({ render }) => (
+    <Mutation
+      mutation={CREATE_ORDER_MUTATION}
+      refetchQueries={[{ query: CURRENT_USER_QUERY }]}
+    >
+      {render}
+    </Mutation>
+  )
+});
+
 function totalItems(cart) {
   return cart.reduce((tally, cartItem) => tally + cartItem.quantity, 0);
 }
 
 class TakeMyMoney extends React.Component {
-  onToken = async (res, createOder) => {
+  onToken = async (res, createOder, toggleCart) => {
+    toggleCart();
+    NProgress.start();
     //manually call the mutation once we have the strip token
     const order = await createOder({
       variables: {
@@ -38,35 +57,33 @@ class TakeMyMoney extends React.Component {
     }).catch(err => {
       alert(err.message);
     });
-    console.log(order);
+    Router.push({
+      pathname: "/order",
+      query: { id: order.data.createOrder.id }
+    });
   };
   render() {
     return (
-      <User>
-        {({ data: { me } }) => (
-          <Mutation
-            mutation={CREATE_ORDER_MUTATION}
-            refetchQueries={[{ query: CURRENT_USER_QUERY }]}
-          >
-            {createOrder => (
-              <StripeCheckout
-                amount={calcTotalPrice(me.cart)}
-                name="Sick Fits"
-                description={`Order of ${totalItems(me.cart)} Items`}
-                image={
-                  me.cart.length && me.cart[0].item && me.cart[0].item.image
-                }
-                stripeKey="pk_test_xI63UZT9OcMc2jtEe7bRBdaz"
-                currency="USD"
-                email={me.email}
-                token={res => this.onToken(res, createOrder)}
-              >
-                {this.props.children}
-              </StripeCheckout>
-            )}
-          </Mutation>
-        )}
-      </User>
+      <Composed>
+        {({ user, toggleCart, createOrder }) => {
+          const me = user.data.me;
+          if (!me) return null;
+          return (
+            <StripeCheckout
+              amount={calcTotalPrice(me.cart)}
+              name="Sick Fits"
+              description={`Order of ${totalItems(me.cart)} Items`}
+              image={me.cart.length && me.cart[0].item && me.cart[0].item.image}
+              stripeKey="pk_test_xI63UZT9OcMc2jtEe7bRBdaz"
+              currency="USD"
+              email={me.email}
+              token={res => this.onToken(res, createOrder, toggleCart)}
+            >
+              {this.props.children}
+            </StripeCheckout>
+          );
+        }}
+      </Composed>
     );
   }
 }
